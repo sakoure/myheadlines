8254c5d85cd6e65fcdee579aca943789afdc999a
sakoure <mbow.mo@gmail.com>
2017-07-22 10:27:46 -0500

    Update headlines.py

--- headlines.py
+++ headlines.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_news():
-  return "no news is good news"
+  return "it is what it is"
 
 if __name__ == '__main__':
   app.run(port=5000, debug=True)
