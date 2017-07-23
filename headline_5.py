76c94084a93d6e7753861f111f98f29e5d469dd4
sakoure <mbow.mo@gmail.com>
2017-07-23 12:26:53 -0500

    Update headline_5.py

--- headline_5.py
+++ headline_5.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from flask import Flask
 import feedparser
+from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,10 +11,14 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="cnn"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+            publication = "bbc"
+    else:
+            publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home5.html", articles=feed['entries'])
-
+    return render_template("home.html",articles=feed['entries']
+                           
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
